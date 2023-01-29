152da639aa897b5b8141610069bb9c4b0499c366
Bar Maor <bmaor2@gmail.com>
2023-01-29 18:08:51 +0200

    bar2

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,55 +1,149 @@
-import axios from "axios";
-import React from "react";
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import "../App.css";
 
-const Login = () => {
-    const [user, setUser] = useState([]);
 
-    const [error, setError] = useState(false)
 
-    const handleSubmit = async (e) => {
-        setUser((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+function HomePage() {
+    const [username, setUsername] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const navigate = useNavigate();
+
+
+    //     const handleChange = e=>{
+    //  how to make it be handleChange${e.taget.type}?
+    //     }
+
+    const handleChange = e => {
         e.preventDefault();
-
-    };
-
-    const handleClick = async (e) => {
-        e.preventDefualt();
-        try {
-            await axios.post(`http://localhost:8800/users/{username}`)
-        } catch (err) {
-            alert("User not found")
-            console.log(err)
-            setError(true)
+        const { name, value } = e.target;
+        switch (name) {
+            case 'password':
+                setPassword(value);
+                break;
+            case 'username':
+                setUsername(value);
+                break;
+            case 'email':
+                setEmail(value);
+                break;
+            case 'phoneNumber':
+                setPhoneNumber(value);
+                break;
+            case 'firstName':
+                setFirstName(value);
+                break;
+            case 'lastName':
+                setLastName(value);
+                break;
+            default:
+                break;
         }
     };
 
-    console.log(user)
-    return (
-        <div className="Login">
-            <h1>Login page </h1>
-            <form>
-                <input
-                    type="text"
-                    placeholder="username"
-                    name="username"
-                    onChange={handleSubmit}
-                />
-                <input
-                    type="password"
-                    placeholder="password"
-                    name="password"
-                    onChange={handleSubmit}
-                />
-                <button onClick={handleClick}>Login</button>
-                {error && "somthing went Wrong!" && <p>{error}</p>}
-                <span>
-                    Don't have an account? <Link to="/register">Register</Link>
-                </span>
-            </form>
-        </div>
-    );
-};
 
-export default Login
+    const logInCheck = async (e) => {
+        e.preventDefault();
+        const response = await fetch('http://localhost:8000/users/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            })
+        });
+        let data = await response.json();
+        console.log(data.message);
+        if (data.isOK) {
+            alert(data.message)
+            const respone = await fetch(`http://localhost:8000/users?user=${username}`)
+            data = await respone.json();
+            localStorage.setItem('userOnline', JSON.stringify({ username: username, user_id: data.user_id }));
+            navigate("/")
+        } else {
+            navigate('/login')
+            alert(data.message)
+        }
+    }
+
+
+    const register = async (e) => {
+        e.preventDefault()
+        const response = await fetch('http://localhost:8000/users/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+                email: email,
+                phoneNumber: phoneNumber,
+                firstName: firstName,
+                lastName: lastName
+            })
+        });
+        let data = await response.json();
+        console.log(data)
+        if (data.isOK) {
+            window.location.reload()
+            return;
+        } else {
+            alert(data.message)
+            navigate('/login')
+        }
+    }
+    // const getUserId = async () => {
+
+    // }
+
+
+
+    return (
+        <div className='container'>
+            <form className='signUp'>
+                <h3>Register</h3>
+                <input onChange={handleChange} type="text" name='firstName' value={firstName} required placeholder="First Name:" />
+                <br />
+                <input onChange={handleChange} type="text" name='lastName' value={lastName} required placeholder="Last Name:" />
+                <br />
+                <input onChange={handleChange} type="text" name='username' value={username} required placeholder="User Name:" />
+                <br />
+                <input onChange={handleChange} type="password" name='password' value={password} required placeholder="Password:" />
+                <br />
+                <input onChange={handleChange} type="email" name='email' value={email} required placeholder="Email:" />
+                <br />
+                <input onChange={handleChange} type="text" name='phoneNumber' value={phoneNumber} required placeholder="Phone Number:" />
+                <br />
+                <button className="form-btn sx log-in" onClick={() => {
+                    document.getElementsByClassName("signIn")[0].className = "signIn active-sx";
+                    document.getElementsByClassName("signUp")[0].className = "signUp inactive-dx";
+                }} type="button">Log In</button>
+                <button className='form-btn dx' onClick={register}> Register </button>
+            </form>
+            <form className='signIn active-dx'>
+                <h3>Log In</h3>
+                <input className='w100' name='username' onChange={handleChange} type="text" value={username.value} placeholder="User Name:" />
+                <br />
+                <input className='w100'name='password' onChange={handleChange} type="password" value={password.value} placeholder="Password:" />
+                <br />
+                <button className="form-btn sx back" onClick={() => {
+                    document.getElementsByClassName("signUp")[0].className = "signUp active-dx";
+                    document.getElementsByClassName("signIn")[0].className = "signIn inactive-sx";
+                }} type="button">Register</button>
+                <button className='form-btn dx' onClick={logInCheck}> Log-in </button>
+            </form>
+
+
+        </div>
+    )
+}
+
+export default HomePage;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,69 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var fs = require("fs");
+const sql = require('mysql');
+
+//connection with database todos
+const con = sql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'z10mz10m',
+  database: "rom_market"
+})
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get('/', (req, res) => {
+  let sql = `select * from user where username = '${req.query.user}'`
+  con.query(sql, (err, result) => {
+    if (err) { console.log(err); return; }
+    res.send(result[0])
+  })
+})
+
+router.post("/login", function (req, res) {
+  let sql = `SELECT username,password
+  FROM user 
+  JOIN user_info 
+  using(user_id)
+  WHERE username = '${req.body.username}' and password = '${req.body.password}'`
+  con.query(sql, (err, result) => {
+    if (err) {console.log(err); res.status(405).send(JSON.stringify({ isOK: false, message: "One of the fields is incorrect." + err })) }
+    console.log(req.body);
+    if (result[0]) {
+      res.status(200).send(JSON.stringify({ isOK: true, message: "login successfully" }))
+    }
+    else {
+      res.status(404).send(JSON.stringify({ isOK: false, message: "One of the fields is incorrect."}))
+    }
+  })
+});
+
+
+router.post("/register", function (req, res) {
+  let sql = `select username from user where username = '${req.body.username}'`
+  con.query(sql, (err, result) => {
+    if (err) { console.log(err); return; }
+    if (result[0]) {
+      res.status(409).send(JSON.stringify({ isOK: false, message: "This user is already exists." }));
+    }
+    else if (req.body.username === "" || req.body.password === "" || req.body.email === "" || req.body.address === "" || req.body.firstName === "" || req.body.lastName === "") {
+      res.status(406).send(JSON.stringify({ isOK: false, message: "One of the fields is Empty" }));
+    }
+    else {
+      sql = `insert into user (first_name, last_name, username, email, phone_number) values ('${req.body.firstName}','${req.body.lastName}','${req.body.username}', '${req.body.email}', '${req.body.phoneNumber}')`;
+      con.query(sql, (err, result) => {
+        if (err) { console.log(err); res.status(422).send(JSON.stringify({ isOK: false, message: "There was an Error with one of the syntax, try again." })); }
+        else {
+          sql = `insert into user_info (user_id, password) values ('${result.insertId}','${req.body.password}')`;
+          con.query(sql, (err, result) => {
+            if (err) { console.log(err); res.status(406).send(JSON.stringify({ isOK: false, message: "There was an Error with one of the syntax, try again." })); }
+            else res.status(200).send(JSON.stringify({ isOK: true, message: "Registerd successfully!" }));
+          })
+        }
+      })
+    }
+  })
 });
 
 module.exports = router;
